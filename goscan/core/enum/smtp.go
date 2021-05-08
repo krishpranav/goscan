20964a6c60a2ef4ffe708a9c59fb4017e47137f8
krishpranav <krisna.pranav@gmail.com>
2021-05-08 14:14:24 +0530

    update smtp scanning

---
+++ goscan/core/enum/smtp.go
@@ -0,0 +1,33 @@
+package enum
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/krishpranav/goscan/core/utils"
+)
+
+func (s *EnumScan) EnumSMTP() {
+	for _, port := range s.Target.GetPorts(utils.Config.DB) {
+		if port.Status == "open" {
+			service := port.GetService(utils.Config.DB)
+			if port.Number == 25 || strings.Contains(strings.ToLower(service.Name), "smtp") {
+				utils.Config.Log.LogInfo(fmt.Sprintf("Starting Enumeration: %s:%d (%s)", s.Target.Address, port.Number, service.Name))
+
+				// -----------------------------------------------------------------------
+				// NMAP
+				// -----------------------------------------------------------------------
+				name := fmt.Sprintf("%s_smtp_%d_nmap", s.Target.Address, port.Number)
+				nmapArgs := fmt.Sprintf("-sV -Pn --script=smtp-enum-users,smtp-vuln* --script-args='smtp-vuln-cve2010-4344.exploit' -p25,465,587,%d", port.Number)
+				s.runNmap(name, s.Target.Address, "RDP", name, nmapArgs)
+
+				// -----------------------------------------------------------------------
+				// SMTP-USER-ENUM
+				// -----------------------------------------------------------------------
+				output := s.makeOutputPath("SMTP", fmt.Sprintf("%s_smtp_%d_user-enum", s.Target.Address, port.Number))
+				cmd := fmt.Sprintf("smtp-user-enum -M VRFY -U %s -t %s > %s", utils.WORDLIST_SMTP, s.Target.Address, output)
+				s.runCmd(cmd)
+			}
+		}
+	}
+}
