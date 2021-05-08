eb045ef0d4bbc48311f4723acf6d21c76b3c9075
krishpranav <krisna.pranav@gmail.com>
2021-05-08 17:52:02 +0530

    update ssh scanning

---
+++ goscan/core/enum/ssh.go
@@ -0,0 +1,34 @@
+package enum
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/krishpranav/goscan/core/utils"
+)
+
+func (s *EnumScan) EnumSSH() {
+	for _, port := range s.Target.GetPorts(utils.Config.DB) {
+		if port.Status == "open" {
+			service := port.GetService(utils.Config.DB)
+			if port.Number == 22 || port.Number == 2222 || strings.Contains(strings.ToLower(service.Name), "ssh") {
+
+				utils.Config.Log.LogInfo(fmt.Sprintf("Starting Enumeration: %s:%d (%s)", s.Target.Address, port.Number, service.Name))
+
+				// -----------------------------------------------------------------------
+				// HYDRA - NON POLITE
+				// -----------------------------------------------------------------------
+				if s.Polite != "POLITE" {
+					// Build command
+					output := s.makeOutputPath("SSH", fmt.Sprintf("%s_ssh_hydra", s.Target.Address))
+					cmd := fmt.Sprintf("hydra -L %s -P %s -f -o %s -u %s -s %d ssh",
+						utils.WORDLIST_HYDRA_SSH_USER, utils.WORDLIST_HYDRA_SSH_PWD,
+						output,
+						s.Target.Address, port.Number,
+					)
+					s.runCmd(cmd)
+				}
+			}
+		}
+	}
+}
